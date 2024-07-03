414b453ee823cd638dd6595fc4d620a7e571f64a
codewiser116 <benazir.baialieva@gmail.com>
2024-07-03 10:03:10 -0500

    configured cucumber reports

--- src/test/java/runners/CucumberRunner.java
+++ src/test/java/runners/CucumberRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
-        plugin = "html:target/reports/cucumberReport.html",
+        plugin = {"html:target/reports/cucumberReport.html", "json:target/reports/cucumber.json"},
         features = "src/test/resources",
         glue = "steps",
         tags = "@smoke",
